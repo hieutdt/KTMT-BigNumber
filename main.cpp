f3aa1608a1933bc988fb9003b81ae7b7bacfa647
An Vo <votrngan@hotmail.com>
2019-04-01 15:20:20 +0700

    Nhap, xuat file, chuyen sang he hexa
    ManipulationModules la cac ham dung de nhap xuat va xu ly tren QInt, QFloat

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include "QInt.h"
 #include "QFloat.h"
+#include "ManipulationModules.h"
 using namespace std;
 
 int main() {
-	QFloat a, b;
-	cout << "a = ";
-	cin >> a;
-	cout << "So vua nhap: " << a << endl;
-	cout << "b = ";
-	cin >> b;
-	cout << "So vua nhap: " << b << endl;
-	QFloat c = a * b;
-	cout << "a * b = " << c << endl;
-
+	//QFloat a, b;
+	//cout << "a = ";
+	//cin >> a;
+	//cout << "So vua nhap: " << a << endl;
+	//cout << "b = ";
+	//cin >> b;
+	//cout << "So vua nhap: " << b << endl;
+	//QFloat c = a * b;
+	//cout << "a * b = " << c << endl;
+	solveAFile("input.txt", "output.txt");
 	system("pause");
 	return 0;
 }
