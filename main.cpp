2b6f6473e3b74a956dd08b3bb0872434e317a2b7
Lam Thanh Loc <wankygame@gmail.com>
2019-04-02 13:10:47 +0700

    Clean code

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@ using namespace std;
 
 int main() {
 	QFloat a, b;
-	cout << "Nhap a = ";
+	cout << "a = ";
 	cin >> a;
-<<<<<<< HEAD
 	cout << "So vua nhap: " << a << endl;
 	cout << a.toBinary(1) << endl;
 	cout << "b = ";
@@ -18,25 +17,6 @@ int main() {
 	cout << endl;
 	QFloat c = a / b;
 	cout << "a / b = " << c << endl;
-=======
-	cout << "SO vua nhap: a = " << a << endl;
-	/*cout << "So vua nhap a = " << a << endl;
-	cout << "Day bit cua a = " << a.toBinary() << endl;*/
-
-	cout << "Nhap b = ";
-	cin >> b;
-	/*cout << "So vua nhap b = " << b << endl;
-	cout << "Day bit cua b = " << b.toBinary() << endl;*/
-	
-	QFloat c = a * b;
-	cout << "a x b = " << c << endl;
-	c = a - b;
-	cout << "a - b = " << c << endl;
-	c = a + b;
-	cout << "a + b = " << c << endl;
-
-
->>>>>>> 2d9751204fc6e156325cad207560522900fcc68a
 	system("pause");
 	return 0;
 }
